f5769dfd662d8d2c3389eb12993bdd57cdd1ea82
Coders Club <hi@codersclub.com>
2025-02-14 21:39:17 +0300

    create projects page and modified site

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,44 @@
 'use client';
-import { motion } from 'framer-motion';
+import { motion,animate } from 'framer-motion';
 import Link from 'next/link';
 import Image from 'next/image';
 import Photo from "../public/1.jpg"
+import { useEffect, useState } from 'react';
 
 
 export default function Home() {
+
+const [projects,setProjects]=useState(0);
+const [blogs,setBlogs]=useState(0);
+const [certificate,setCertificate]=useState(0);
+
+
+
+
+useEffect(() => {
+  animate(0, 15, {
+    duration:2,
+    ease: "circOut",
+    onUpdate: (latest) => setProjects(Math.round(latest)),
+  });
+  animate(0, 5, {
+    duration:2,
+    ease: "circOut",
+    onUpdate: (latest) => setBlogs(Math.round(latest)),
+  });
+  animate(0, 5, {
+    duration:2,
+    ease: "circOut",
+    onUpdate: (latest) => setCertificate(Math.round(latest)),
+  });
+}, []);
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
       <div className="container mx-auto px-4 py-20">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
+          onUpdate={(latest)=>console.log(latest.x)}
           transition={{ duration: 0.8 }}
           className="text-center"
         >
@@ -40,12 +67,26 @@ export default function Home() {
               </motion.button>
             </Link>
           </div>
-<div className='flex justify-center items-center'>
+<div className='flex justify-center items-center '>
 <Image src={Photo}  className='w-[320px] h-80 rounded-full p-2' alt='photo'></Image>
-
 </div>
 
 
+<div className='flex flex-row justify-center items-center lg:space-x-12 space-x-2 text-4xl  pt-12 text-blue-400   '>
+   <div className=' rounded-md  shadow-2xl p-8 w-44 bg-gray-700 transition delay-100 ease-in-out duration-300 hover:translate-y-1 hover:scale-100 hover:rotate-[360deg]'>
+  {projects} + <span className='text-xl '>Projeler</span>
+   </div>
+
+   <div className='rounded-md shadow-2xl p-8 w-44 bg-gray-700  transition delay-100 ease-in-out duration-300 hover:translate-y-1 hover:scale-100 hover:rotate-[360deg]'>
+  {blogs} + <span className='text-xl '><br></br>Bloglar</span>
+   </div>
+
+   <div className='rounded-md shadow-2xl p-8 w-44 bg-gray-700  transition delay-100 ease-in-out duration-300 hover:translate-y-1 hover:scale-100 hover:rotate-[360deg]'>
+  {certificate} + <span className='text-xl '><br></br>Sertifikalar</span>
+   </div>
+
+   
+</div>
 
 
 
---
+++ app/projects/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import proje1 from "../../public/image.png"
+export default function Projects() {
+  return (
+    <main className="min-h-screen from-gray-900 bg-gradient-to-b to-gray-800 pt-20">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div>
+          <h1 className="text-gray-200 text-3xl pl-16">Project</h1>
+        </div>
+        <div></div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.2 }}
+          className="w-[300px] lg:px-8 px-20 pt-16 flex flex-col gap-6"
+        >
+          <div className="w-[300px] text-gray-200  flex flex-col rounded-md shadow-xl
+          bg-gray-800
+          ">
+         
+           <div><Image src={proje1} alt="project image" className="w-full object-cover rounded-md "  /></div>
+            <div className="flex flex-col p-2">
+            <p className="text-xl flex justify-center items-center ">Portfölyo Sitesi</p>
+          Teknolojiler:  Next js, HTML, Tailwind CSS
+            </div>
+          </div>
+
+        
+        </motion.div>
+      </motion.div>
+    </main>
+  );
+}
