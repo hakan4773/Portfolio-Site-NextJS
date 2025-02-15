05446844e64a5adaa2fd26c091e978db8206a873
Coders Club <hi@codersclub.com>
2025-02-15 23:38:52 +0300

    created libs,models and api folders

---
+++ app/models/portfolyo.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const PortfolioSchema=new mongoose.Schema({
+
+title:{type:String,required:true},
+description:{type:String,required:true},
+
+createdAt:{type:Date,default:Date.now}
+
+})
+export default mongoose.models.Portfolio || mongoose.model('Portfolio', PortfolioSchema);
