b83b706804837a689b89d41b92901b9c6d05d8ef
hakan4773 <hb480312@gmail.com>
2025-02-18 00:16:12 +0300

    delete images and update projects cards

--- app/models/portfolyo.js
+++ app/models/portfolyo.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const PortfolioSchema=new mongoose.Schema({
 
 title:{type:String,required:true},
+technologies:{type:String,required:true},
 description:{type:String,required:true},
 imageUrl:{type:String,required:true},
 createdAt:{type:Date,default:Date.now}
